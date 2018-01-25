fe88e9e7f56dbf9f2056f1e819c25b258781f473
vinitha32 <35530364+vinitha32@users.noreply.github.com>
2018-01-24 18:27:27 -0600

    Add files via upload

---
+++ Grocerylist.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.Scanner;
+
+public class Grocerylist {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Please list three items on your grocery shopping list.");
+        Scanner scan = new Scanner(System.in);
+        int i;
+        System.out.println("Item 1 ?"  );
+        i = scan.nextInt();
+
+    }
+
+}
