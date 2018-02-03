3f857706cfcb95bb154951b502b59a76c26565b0
vinitha32 <35530364+vinitha32@users.noreply.github.com>
2018-02-02 19:38:06 -0600

    Add files via upload

---
+++ verseandchorus.java
@@ -0,0 +1,54 @@
+package com.company;
+import java.util.Scanner;
+public class verseandchorus
+{
+  public static void song()
+  {
+    System.out.print("This old man,he played ");
+  }
+  public static void so()
+  {
+    System.out.print("He played knick-knack on my ");
+  }
+  public static void s()
+  {
+    System.out.println("Give the dog a bone,");
+    System.out.println("This old man came rolling home.");
+    System.out.println();
+  }
+
+  public static void main(String[] args)
+  {
+    song();
+    System.out.println("one.");
+    so();
+    System.out.println("thumb;");
+    s();
+    song();
+    System.out.println("two.");
+    so();
+    System.out.println("shoe;");
+    s();
+    song();
+    System.out.println("three.");
+    so();
+    System.out.println("knee;");
+    s();
+    song();
+    System.out.println("four.");
+    so();
+    System.out.println("door;");
+    s();
+    song();
+    System.out.println("five.");
+    so();
+    System.out.println("hive;");
+    s();
+    song();
+    System.out.println("six.");
+    so();
+    System.out.println("sticks;");
+    s();
+
+  }}
+
