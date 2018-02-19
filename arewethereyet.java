84605364607a7aeb272193784f221a4a82799a75
vinitha32 <35530364+vinitha32@users.noreply.github.com>
2018-02-19 14:38:05 -0600

    are we there yet?

---
+++ arewethereyet.java
@@ -0,0 +1,22 @@
+
+import static java.lang.System.out;
+import java.util.Scanner;
+import java.util.*;
+public class arewethereyet
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String s = "";
+        boolean answer = true;
+        while (answer) {
+            System.out.println("are we there yet?");
+            s = sc.nextLine();
+            if ((s).equals("yes")) {
+                System.out.println("yay");
+                answer = false;
+            } else if (!(s).equals("no")) {
+                System.out.println(" Huh? I didn't understand ");
+                answer=false;
+            }
+        }}}
