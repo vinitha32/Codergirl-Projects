aa4ace1404b91d2bc4c51d43625ae4c756e49188
vinitha32 <35530364+vinitha32@users.noreply.github.com>
2018-03-07 13:04:03 -0600

    updated multiplication.

--- multiplication.java
+++ multiplication.java
@@ -6,15 +6,17 @@ public class multiplication {
         System.out.println("How large would you like to see them?");
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
-    if(size<=20)
-    {
-        int size1,size2,result;
+        if (size <= 20) {
+            int size1, size2, result;
 
-       for(size1=0;size1<=size;size1++)
-       {
-         for(size2=0;size2<=size;size2++)
-         {
-         result=size1*size2;
-         System.out.println(size1 + "*" + size2 + "=" + result);
+            for (size1 = 0; size1 <= size; size1++) {
+                for (size2 = 0; size2 <= size; size2++) {
+                    result = size1 * size2;
+                    System.out.println(size1 + "*" + size2 + "=" + result);
 
-       }}}}}
+                }
+            }
+        } else {
+            System.out.println("Choose size not more than 20");
+        }
+    }}
