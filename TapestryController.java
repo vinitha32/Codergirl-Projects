d39acc0ba9ae2a456ae7c5d8ccea893155071579
vinitha32 <35530364+vinitha32@users.noreply.github.com>
2018-05-30 18:17:49 -0500

    Update TapestryController.java

--- TapestryController.java
+++ TapestryController.java
@@ -1,109 +1,43 @@
 package PatternMaker;
-import java.util.Random;
 
-public class TapestryController {
+
+
+
+public class TapestaryController {
     public static void main(String[] args) {
 
-    CrossStitchWigglePattern p=new CrossStitchWigglePattern();
-        boolean needleJam = (Math.random() < 0.40d);
-        if(needleJam)
-        {
-        for (int j = 1; j <= 3; j++) {
-            for (int i = 1; i <= 7; i++) {
-                System.out.print(p.pattern1);
-            }
-            System.out.println(p.pattern1);
-        }
-        System.out.println("------------------");
-        System.out.println("------------------");
 
-        for (int k = 1; k <= 8; k++) {
-            for (int c = 1; c <= 5; c++) {
-                System.out.print(p.pattern1);
-            }
-            System.out.println(p.pattern1);
-        }}
+        PatternClass cross = new CrossStitchWigglePattern();
+        PatternClass zig = new ZigZagLinePattern();
+        PatternClass bauble = new BaublePattern();
+        PatternClass blocky = new BlockyBaublePattern();
+        PatternClass broken = new BrokenWigglePattern();
+        PatternClass[] c = {cross, zig, bauble, blocky, broken};
 
 
-    ZigZagLinePattern z=new ZigZagLinePattern();
+        for (int m = 0; m < c.length; m++) {
 
-        boolean needleJam1 = (Math.random() < 0.00d);
-        if(needleJam1)
-                for (int j = 1; j <= 3; j++) {
-                    for (int i = 1; i <= 7; i++) {
-                        System.out.print(z.pattern2);
-                    }
-                    System.out.println(z.pattern2);
+            System.out.println("Printing "+c[m]);
+            
+            for (int j = 1; j <= 3; j++) {
+                for (int i = 1; i <= 7; i++) {
+                    System.out.print(c[m].pattern());
                 }
-                System.out.println("-------------------------------------");
-                System.out.println("-------------------------------------");
+                System.out.println(c[m].pattern());
+            }
 
-                for (int k = 1; k <= 8; k++) {
-                    for (int c = 1; c <= 5; c++) {
-                        System.out.print(z.pattern2);
-                    }
-                    System.out.println(z.pattern2);
+            System.out.println("-----------------------");
+            System.out.println("-----------------------");
+            {
+            for (int p = 1; p <= 8; p++) {
+                for (int l = 1; l <= 5; l++) {
+                    System.out.print(c[m].pattern());
                 }
-
-
-
-            BaublePattern b=new BaublePattern();
-        boolean needleJam2 = (Math.random() < 0.00d);
-        if(needleJam2)
-                for (int j = 1; j <= 3; j++) {
-                    for (int i = 1; i <= 7; i++) {
-                        System.out.print(b.pattern3);
-                    }
-                    System.out.println(b.pattern3);
-                }
-                System.out.println("------------------");
-                System.out.println("------------------");
-
-                for (int k = 1; k <= 8; k++) {
-                    for (int c = 1; c <= 5; c++) {
-                        System.out.print(b.pattern3);
-                    }
-                    System.out.println(b.pattern3);
-                }
-
-
-BlockyBaublePattern bp=new BlockyBaublePattern();
-        boolean needleJam3 = (Math.random() < 0.10d);
-if(needleJam3)
-        for (int j = 1; j <= 3; j++) {
-            for (int i = 1; i <= 7; i++) {
-                System.out.print(bp.pattern4);
+                System.out.println(c[m].pattern());
             }
-            System.out.println(bp.pattern4);
-        }
-        System.out.println("------------------");
-        System.out.println("------------------");
-
-        for (int k = 1; k <= 8; k++) {
-            for (int c = 1; c <= 5; c++) {
-                System.out.print(bp.pattern4);
-            }
-            System.out.println(bp.pattern4);
 
         }
 
+        System.out.println();
 
-    BrokenWigglePattern n=new BrokenWigglePattern();
-        boolean needleJam4 = (Math.random() < 0.40d);
-if(needleJam4)
-        for (int j = 1; j <= 3; j++) {
-            for (int i = 1; i <= 7; i++) {
-                System.out.print(n.pattern5);
-            }
-            System.out.println(n.pattern5);
-        }
-        System.out.println("------------------");
-        System.out.println("------------------");
-
-        for (int k = 1; k <= 8; k++) {
-            for (int c = 1; c <= 5; c++) {
-                System.out.print(n.pattern5);
-            }
-            System.out.println(n.pattern5);
-        }
-    }}
+    }}}
