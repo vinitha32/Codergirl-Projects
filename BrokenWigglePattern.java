763011c63c33a0a7c0427b7074c256d02162edcb
vinitha32 <35530364+vinitha32@users.noreply.github.com>
2018-05-31 11:31:45 -0500

    Update BrokenWigglePattern.java

--- BrokenWigglePattern.java
+++ BrokenWigglePattern.java
@@ -4,9 +4,11 @@ package PatternMaker;
 public class BrokenWigglePattern extends PatternClass {
 
    public String pattern() {
-
-
-
     return pattern5;
 
-}}
+}
+    public boolean isJammed() {
+
+        return s[4];
+    }
+}
