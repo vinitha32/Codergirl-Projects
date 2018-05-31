314f431be24e39435b9a9251b066e6c53868622b
vinitha32 <35530364+vinitha32@users.noreply.github.com>
2018-05-31 09:25:41 -0500

    Update User.java

--- User.java
+++ User.java
@@ -1,41 +1,41 @@
 package MicroBlog2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+
 
 public class User {
 
     String firstname;
     String lastname;
-    String webaddress;
-    String email ;
-   String username;
+    String url;
+    String email;
+    String username;
+
+    public User(String username, String firstname, String lastname, String url, String email)
+
+    {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.url = url;
+        this.email = email;
+        this.username = username;
 
 
-public User(String username,String firstname,String lastname,String webaddress,String email )
-{
-    this.firstname=firstname;
-    this.lastname=lastname;
-    this.webaddress=webaddress;
-    this.email=email;
-    this.username=username;
 
+    }
+
+    public String getUser() {
+        return " FirstName "+firstname +" Last Name "+ lastname +" URL "+ url +" Email "+ email;
+    }
+
+
+
+
+    public String getUsername() {
+        return username;
+    }
 
 }
 
-public String getUser()
-{
-        return username+firstname + lastname +  webaddress + email;
-    }
-
-    public String getUsername()
-  {
-    return username;
-   }
-
-
-
-  }
 
 
 
