a0f2f9d34ac46f1fa02b0c604ad9d199eb4edc62
vinitha32 <35530364+vinitha32@users.noreply.github.com>
2018-05-31 11:33:35 -0500

    Update CrossStitchWigglePattern.java

--- CrossStitchWigglePattern.java
+++ CrossStitchWigglePattern.java
@@ -1,9 +1,16 @@
 package PatternMaker;
 
 public  class CrossStitchWigglePattern extends PatternClass {
-    
+
 public String pattern() {
-    
+
 
 return pattern1;
-}}
+}
+public boolean isJammed()
+{
+
+    return s[0];
+}
+
+}
