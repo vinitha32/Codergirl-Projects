051f47feba481c7738886a21e02c1d41fdbb071c
vinitha32 <35530364+vinitha32@users.noreply.github.com>
2018-05-30 19:06:25 -0500

    Update BlockyBaublePattern.java

--- BlockyBaublePattern.java
+++ BlockyBaublePattern.java
@@ -1,6 +1,9 @@
 package PatternMaker;
 
 public class BlockyBaublePattern extends PatternClass {
-public void pattern() {
-    System.out.println(pattern4);
-}}
+
+    public String pattern() {
+
+        return pattern4;
+
+    }}
