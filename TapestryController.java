a101a00420bf5813f26320d7fb10ba411af5b69d
vinitha32 <35530364+vinitha32@users.noreply.github.com>
2018-05-30 19:02:20 -0500

    Update TapestryController.java

--- TapestryController.java
+++ TapestryController.java
@@ -6,7 +6,11 @@ package PatternMaker;
 public class TapestaryController {
     public static void main(String[] args) {
 
-
+        boolean needleJam = (Math.random() < 0.05d);
+        boolean needleJam1 =true; //(Math.random() < 0.00d);
+        boolean needleJam2 = true;//(Math.random() < 0.00d);
+        boolean needleJam3 = (Math.random() < 0.10d);
+        boolean needleJam4 = (Math.random() < 0.40d);
         PatternClass cross = new CrossStitchWigglePattern();
         PatternClass zig = new ZigZagLinePattern();
         PatternClass bauble = new BaublePattern();
@@ -14,30 +18,37 @@ public class TapestaryController {
         PatternClass broken = new BrokenWigglePattern();
         PatternClass[] c = {cross, zig, bauble, blocky, broken};
 
+        boolean[] s={needleJam,needleJam1,needleJam2,needleJam3,needleJam4};
 
+        boolean status=false;
+
+
+{
         for (int m = 0; m < c.length; m++) {
 
             System.out.println("Printing "+c[m]);
-            
+            if(s[m]){
             for (int j = 1; j <= 3; j++) {
                 for (int i = 1; i <= 7; i++) {
                     System.out.print(c[m].pattern());
                 }
                 System.out.println(c[m].pattern());
-            }
+            }}
 
             System.out.println("-----------------------");
             System.out.println("-----------------------");
             {
-            for (int p = 1; p <= 8; p++) {
-                for (int l = 1; l <= 5; l++) {
-                    System.out.print(c[m].pattern());
+                if (s[m]) {
+                    for (int p = 1; p <= 8; p++) {
+                        for (int l = 1; l <= 5; l++) {
+                            System.out.print(c[m].pattern());
+                        }
+                        System.out.println(c[m].pattern());
+                        status = true;
+                    }
+
                 }
-                System.out.println(c[m].pattern());
             }
-
-        }
-
         System.out.println();
 
-    }}}
+    }}}}
