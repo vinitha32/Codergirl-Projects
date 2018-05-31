a129554e38942dd399d72137efdb2c513337ee8c
vinitha32 <35530364+vinitha32@users.noreply.github.com>
2018-05-31 11:36:48 -0500

    Update SmallCrossStitch.java

--- SmallCrossStitch.java
+++ SmallCrossStitch.java
@@ -2,8 +2,10 @@ package PatternMaker;
 
 public class SmallCrossStitch extends Stitch
 {
-    public char stitch1(char stitch1)
+
+
+    public char stitch()
     {
-        return stitch1;
+        return this.stitch1;
     }
 }
