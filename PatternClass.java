e3558ad8c26c6e21f6129bccb59e0aeb4a9fbd0d
vinitha32 <35530364+vinitha32@users.noreply.github.com>
2018-06-13 20:03:11 -0500

    Update PatternClass.java

--- PatternClass.java
+++ PatternClass.java
@@ -14,21 +14,7 @@ public abstract class PatternClass {
 
     abstract String pattern();
     abstract boolean isJammed();
-   boolean needleJam = (Math.random() < 0.05d);
-    boolean needleJam1 = true;
-    boolean needleJam2 = true;
-    boolean needleJam3 = (Math.random() < 0.10d);
-    boolean needleJam4 = (Math.random() < 0.40d);
-
-
-    boolean[] s={needleJam,needleJam1,needleJam2,needleJam3,needleJam4};
-
-    String  pattern1 = String.valueOf(small.stitch())+String.valueOf(large.stitch());
-     String   pattern2=String.valueOf(zig.stitch()) +String.valueOf(zig.stitch())+String.valueOf(zig.stitch())+String.valueOf(fill.stitch())+String.valueOf(fill.stitch())+String.valueOf(fill.stitch());
-    String  pattern3=String.valueOf(short1.stitch()) +String.valueOf(short1.stitch()) +String.valueOf(circle.stitch());
-    String  pattern4=String.valueOf(block.stitch())+String.valueOf(block.stitch6) +String.valueOf(short1.stitch())+String.valueOf(short1.dStitch)+String.valueOf(fill.stitch3);
-    String  pattern5=String.valueOf(small.stitch()) +String.valueOf(large.stitch());
-
 
 
 }
+
