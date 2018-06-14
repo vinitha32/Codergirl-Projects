412a4840eab5083909efa02ba27e4542f8c89b9d
vinitha32 <35530364+vinitha32@users.noreply.github.com>
2018-06-13 20:03:52 -0500

    Update BaublePattern.java

--- BaublePattern.java
+++ BaublePattern.java
@@ -3,13 +3,14 @@ package PatternMaker;
 public class BaublePattern extends PatternClass {
 
    public String pattern() {
-
+      String  pattern3=String.valueOf(short1.stitch()) +String.valueOf(short1.stitch()) +String.valueOf(circle.stitch());
       return pattern3;
 
    }
+   boolean needleJam2 = true;
    public boolean isJammed()
    {
 
-      return s[2];
+      return needleJam2;
    }
 }
